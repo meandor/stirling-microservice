fde46e5639dbaee142fa8c44f3e6cba7d0fcd1b9
Daniel Schruhl <danielschruhl@gmail.com>
2017-01-27 22:34:15 +0100

    Add simple test

--- tests/Core/RouterTest.php
+++ tests/Core/RouterTest.php
@@ -5,16 +5,25 @@ use PHPUnit\Framework\TestCase;
 
 class RouterTest extends TestCase
 {
-    public function testPushAndPop()
+    public function testAddRoute()
     {
-        $stack = [];
-        $this->assertEquals(0, count($stack));
+        Router::add("foobar", function () {
+            return "1337";
+        });
+        $actual = Router::$routes;
+        $this->assertEquals(1, count($actual));
+        $this->assertEquals("foobar", $actual[0]["expression"]);
+        $this->assertNotEmpty($actual[0]["function"]);
+        $this->assertEquals("1337", call_user_func($actual[0]["function"]));
+    }
 
-        array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack) - 1]);
-        $this->assertEquals(1, count($stack));
-
-        $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(0, count($stack));
+    public function testAdd404Route()
+    {
+        Router::add404(function () {
+            return "404";
+        });
+        $actual = Router::$routes404;
+        $this->assertEquals(1, count($actual));
+        $this->assertEquals("404", call_user_func($actual[0]));
     }
 }
